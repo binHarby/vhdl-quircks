02888cefe7fe5bb720628a5c9a65ac6772574502
binHarby <lance.alameri@gmail.com>
2022-12-05 13:26:34 +0400

    adding comments to reportAndWaitStatement.vhd

--- reportAndWaitStatment.vhd
+++ reportAndWaitStatment.vhd
@@ -26,6 +26,15 @@ BEGIN
 
 		REPORT "Hello VHDL";
 		wait until b='1';
+		--using a :
+		-- unconditional wait like
+		-- wait;
+		-- or a wait for like
+		-- wait for 10 ns;
+		-- can't be synthizised , and thus is not valid code for a hardware connection, but maybe for a simulation
+
+
+		-- for a real time delay, consider a delay generator, where you use delay cycle= #OfSeconds/ClockPeriod, where clock period is of course 1/Clockfrequency
 		c<=a and b;
 		
 	
